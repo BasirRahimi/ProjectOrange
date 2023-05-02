96ef46643bad132f5b1a0f9f17d5f60be1ac616c
Basir Rahimi <basiralirahimi@gmail.com>
2023-05-02 16:38:38 +0100

    dockerise app and default tests working

--- resources/js/views/Dashboard/UserManagement.vue
+++ resources/js/views/Dashboard/UserManagement.vue
@@ -1,81 +1,83 @@
 <template>
-    <ContentBox title="Users requesting access" v-if="requestingAccess.length">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="user in requestingAccess">
-                    <td>{{ user.name }}</td>
-                    <td>{{ user.company }}</td>
-                    <td>{{ user.email }}</td>
-                    <td>{{ user.role_name }}</td>
-                    <td>
-                        <BaseDropdown menu-classes="shadow">
-                            <li class="dropdown-item">
-                                <a href="#" @click="" class="text-default">
-                                    Change Role
-                                </a>
-                            </li>
-                            <li class="dropdown-item">
-                                <a href="#" @click="" class="text-default">
-                                    Grant Access
-                                </a>
-                            </li>
-                            <li class="dropdown-item">
-                                <a href="#" @click="" class="text-default">
-                                    Delete Request
-                                </a>
-                            </li>
-                        </BaseDropdown>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </ContentBox>
-    <ContentBox title="Users">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="user in users">
-                    <td>{{ user.id }}</td>
-                    <td>{{ user.name }}</td>
-                    <td>{{ user.company }}</td>
-                    <td>{{ user.email }}</td>
-                    <td>{{ user.role_name }}</td>
-                    <td>
-                        <BaseDropdown menu-classes="shadow">
-                            <li class="dropdown-item">
-                                <a href="#" class="text-default" @click="">
-                                    Change Role
-                                </a>
-                            </li>
-                            <li class="dropdown-item">
-                                <a href="#" class="text-default" @click="">
-                                    Revoke Access
-                                </a>
-                            </li>
-                        </BaseDropdown>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </ContentBox>
+    <div>
+        <ContentBox title="Users requesting access" v-if="requestingAccess.length">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Company</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="user in requestingAccess">
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.company }}</td>
+                        <td>{{ user.email }}</td>
+                        <td>{{ user.role_name }}</td>
+                        <td>
+                            <BaseDropdown menu-classes="shadow">
+                                <li class="dropdown-item">
+                                    <a href="#" @click="" class="text-default">
+                                        Change Role
+                                    </a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a href="#" @click="" class="text-default">
+                                        Grant Access
+                                    </a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a href="#" @click="" class="text-default">
+                                        Delete Request
+                                    </a>
+                                </li>
+                            </BaseDropdown>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </ContentBox>
+        <ContentBox title="Users">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Company</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="user in users">
+                        <td>{{ user.id }}</td>
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.company }}</td>
+                        <td>{{ user.email }}</td>
+                        <td>{{ user.role_name }}</td>
+                        <td>
+                            <BaseDropdown menu-classes="shadow">
+                                <li class="dropdown-item">
+                                    <a href="#" class="text-default" @click="">
+                                        Change Role
+                                    </a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a href="#" class="text-default" @click="">
+                                        Revoke Access
+                                    </a>
+                                </li>
+                            </BaseDropdown>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </ContentBox>
+    </div>
 </template>
 <script setup>
 import axios from 'axios';
